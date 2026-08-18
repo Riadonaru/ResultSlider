012a57137956474ba44dbf5c455d550c1d83a42d
liador <liador@MacBook-Air-sl-Liad.local>
2026-08-18 20:27:48 +0200

    more making of variables into class attributes

--- src/functions/DataApp.m
+++ src/functions/DataApp.m
@@ -2,6 +2,8 @@ classdef DataApp < handle
     % The '< handle' part is critical so updates modify the original object
 
     properties (SetAccess = private)
+        FigH = 800;
+        FigW = 1200;
         PlotColor = 'blue'
         StartFreq
         EndFreq
@@ -12,8 +14,12 @@ classdef DataApp < handle
     end
     
     properties (Access = public)
-        % Anything can read and write to these normally
-        UILabel
+        Figure
+        TabGroup
+        Tab1
+        Tab2
+        GridLayout
+        Axis
     end
 
     methods
@@ -26,34 +32,27 @@ classdef DataApp < handle
             obj.StepSize = vars.stepSize;
             obj.NumMeasurements = vars.numMeasurements;
             obj.Data = vars.data;
+
+            obj.Figure = uifigure('Name', 'Results', ...
+                           'Visible', 'On', ...
+                           'Position', [100, 100, obj.FigW, obj.FigH]);
+
+            obj.TabGroup = uitabgroup(obj.Figure, 'Units', 'Normalized', 'Position', [0 0 1 1]);
+            obj.Tab1 = uitab(obj.TabGroup, 'Title', 'Data View');
+            obj.Tab2 = uitab(obj.TabGroup, 'Title', 'Settings');
+
+            obj.GridLayout = uigridlayout(obj.Tab1, [2, 1]);
+            obj.GridLayout.RowHeight = {'10x', '1x'};
+            obj.GridLayout.ColumnWidth = {'1x'};
+            obj.Axis = uiaxes(obj.GridLayout);
+            obj.Axis.Layout.Row = 1;
+            obj.Axis.Layout.Column = 1;
+
         end
 
         function show(obj)
 
             global HALF_CM INITIAL_FREQUENCY INITIAL_POINT DISPLAYED_SCANS SPECTRUM PHASE;
-
-            data = obj.Data;
-
-            fig_h = 800;
-            fig_w = 1200;
-            fig = uifigure('Name', 'Results', 'Position', [100, 100, fig_w, fig_h]);
-
-            % 2. Create the tab group as a child of the figure
-            tgroup = uitabgroup(fig, 'Units', 'Normalized', 'Position', [0 0 1 1]);
-
-            % 3. Create the individual tabs as children of the tab group
-            tab1 = uitab(tgroup, 'Title', 'Data View');
-            tab2 = uitab(tgroup, 'Title', 'Settings');
-
-            %% ==================== TAB 1 ====================
-            g = uigridlayout(tab1, [2, 1]);
-            g.RowHeight = {'10x', '1x'};
-            g.ColumnWidth = {'1x'};
-
-            % Create axes inside the grid and center it
-            ax = uiaxes(g);
-            ax.Layout.Row = 1;
-            ax.Layout.Column = 1;
 
             y_vals = obj.Data{PHASE + 1};
 
@@ -92,72 +91,72 @@ classdef DataApp < handle
             settings = plot_settings{SPECTRUM + 1};
 
             % Initial plot
-            plot(ax, settings{7}, settings{8}, 'LineWidth', 2);
-            xlim(ax, settings{1});
+            plot(obj.Axis, settings{7}, settings{8}, 'LineWidth', 2);
+            xlim(obj.Axis, settings{1});
             info = cell(1, length(DISPLAYED_SCANS));
             for i = 1:length(DISPLAYED_SCANS)
                 info{i} = ['Scan ' num2str(DISPLAYED_SCANS(i))];
             end
-            legend(ax, info, 'Location', 'northeastoutside', 'FontSize', 16);
-            title(ax, settings{5}, ...
+            legend(obj.Axis, info, 'Location', 'northeastoutside', 'FontSize', 16);
+            title(obj.Axis, settings{5}, ...
                 'FontSize', 24, ...
                 'FontWeight','Bold', ...
                 'FontName', 'Times New Roman');
 
-            sld = uislider(g, ...
+            sld = uislider(obj.GridLayout, ...
                 'Position', [100, 50, 450, 3], ...
                 'Limits', settings{2}, ...
                 'Value', settings{6}, ...
                 'Step', settings{3}, ...
-                'ValueChangedFcn', @(sld, event) obj.sliderUpdate(sld, ax, plot_settings));
+                'ValueChangedFcn', @(sld, event) obj.sliderUpdate(sld, plot_settings));
             sld.Layout.Row = 2;
             sld.Layout.Column = 1;
 
             %% ==================== TAB 2 ====================
 
             uilabel('Text', 'Settings', ...
-                'Parent', tab2, ...
+                'Parent', obj.Tab2, ...
                 'FontSize', 40, ...
                 'FontWeight', 'Bold', ...
-                'Position', [50 fig_h - 100 200 50]);
+                'Position', [50 obj.FigH - 100 200 50]);
 
             uilabel('Text', 'Plot Settings', ...
-                'Parent', tab2, ...
+                'Parent', obj.Tab2, ...
                 'FontSize', 16, ...
                 'FontWeight', 'Bold', ...
-                'Position', [50 fig_h - 180 200 50]);
+                'Position', [50 obj.FigH - 180 200 50]);
 
             hiddenStates = {' ', ' '};
             labelTexts = {'Intensity along the slit', 'Spectrum at point'};
-            lbl = uilabel(tab2, ...
+            lbl = uilabel(obj.Tab2, ...
                 'Text', labelTexts{SPECTRUM + 1}, ...
-                'Position', [105, fig_h - 220, 200, 20]);
+                'Position', [105, obj.FigH - 220, 200, 20]);
 
-            uiswitch(tab2, 'slider', ...
+            uiswitch(obj.Tab2, 'slider', ...
                 'Value', 'Off', ...
-                'ValueChangedFcn', @(src, event) obj.modifyLayout(ax, lbl, labelTexts, sld, plot_settings), ...
-                'Position', [50, fig_h - 220, 45, 20], ...
+                'ValueChangedFcn', @(src, event) obj.modifyLayout(lbl, labelTexts, sld, plot_settings), ...
+                'Position', [50, obj.FigH - 220, 45, 20], ...
                 'Items', hiddenStates, ...
                 'Value', hiddenStates{SPECTRUM + 1});
 
             hiddenStates1 = {' ', ' '};
             labelTexts1 = {'Magnitude', 'Phase'};
-            lbl1 = uilabel(tab2, ...
+            lbl1 = uilabel(obj.Tab2, ...
                 'Text', labelTexts1{PHASE + 1}, ...
-                'Position', [105, fig_h - 250, 200, 20]);
+                'Position', [105, obj.FigH - 250, 200, 20]);
 
-            uiswitch(tab2, 'slider', ...
+            uiswitch(obj.Tab2, 'slider', ...
                 'Value', 'Off', ...
-                'ValueChangedFcn', @(src, event) obj.modifyY(ax, lbl1, labelTexts1, plot_settings, sld), ...
-                'Position', [50, fig_h - 250, 45, 20], ...
+                'ValueChangedFcn', @(src, event) obj.modifyY(lbl1, labelTexts1, plot_settings, sld), ...
+                'Position', [50, obj.FigH - 250, 45, 20], ...
                 'Items', hiddenStates1, ...
                 'Value', hiddenStates{PHASE + 1});
 
             uilabel('Text', 'Available Scans', ...
-                'Parent', tab2, ...
+                'Parent', obj.Tab2, ...
                 'FontSize', 16, ...
                 'FontWeight', 'Bold', ...
-                'Position', [550 fig_h - 180 200 50]);
+                'Position', [550 obj.FigH - 180 200 50]);
 
             for num = 1:length(y_vals)
                 value = 0;
@@ -165,16 +164,16 @@ classdef DataApp < handle
                     value = 1;
                 end
                 uicheckbox('Text', ['  Scan' num2str(num)], ...
-                    'Parent', tab2, ...
+                    'Parent', obj.Tab2, ...
                     'Value', value, ...
                     'UserData', num, ...
-                    'ValueChangedFcn', @(src, event) obj.updateScan(src, ax, sld, plot_settings), ...
-                    'Position', [550 fig_h - (200+30*num) 200 50]);
+                    'ValueChangedFcn', @(src, event) obj.updateScan(src, sld, plot_settings), ...
+                    'Position', [550 obj.FigH - (200+30*num) 200 50]);
             end
         end
 
         % Called each time a plot update is required
-        function updatePlot(obj, ax, index, plot_settings)
+        function updatePlot(obj, index, plot_settings)
             global SPECTRUM DISPLAYED_SCANS PHASE;
 
             y_vals = obj.Data{PHASE + 1};
@@ -184,48 +183,48 @@ classdef DataApp < handle
                 y = extractHeights(y_vals, DISPLAYED_SCANS, index);
             end
             settings = plot_settings{SPECTRUM + 1};
-            cla(ax);
-            plot(ax, settings{7}, y, 'LineWidth', 2);
+            cla(obj.Axis);
+            plot(obj.Axis, settings{7}, y, 'LineWidth', 2);
             info = cell(1, length(DISPLAYED_SCANS));
             for i = 1:length(DISPLAYED_SCANS)
                 info{i} = ['Scan ' num2str(DISPLAYED_SCANS(i))];
             end
-            legend(ax, info, 'Location', 'northeastoutside', 'FontSize', 16);
+            legend(obj.Axis, info, 'Location', 'northeastoutside', 'FontSize', 16);
         end
 
 
         % Handler for the slider
-        function sliderUpdate(obj, sld, ax, plot_settings)
+        function sliderUpdate(obj, sld, plot_settings)
 
             global SPECTRUM DISPLAYED_SCANS;
 
             if SPECTRUM == 1
                 index =  2 * sld.Value + 1; % Get current slider value\
-                title(ax, ['Spectrum @ ', num2str((index - 1)/ 2), ' [cm] from Origin']); % In spectrum at point title is point
+                title(obj.Axis, ['Spectrum @ ', num2str((index - 1)/ 2), ' [cm] from Origin']); % In spectrum at point title is point
             else
                 index = sld.Value; % Get current slider value\
                 freq = obj.StartFreq + (index - 1) * (obj.EndFreq - obj.StartFreq) / (obj.NumPoints - 1); % In frequency along the axis title is frequency
-                title(ax, ['Intensity along the Slit @ ', num2str(freq), ' [hz]']); % Update title
+                title(obj.Axis, ['Intensity along the Slit @ ', num2str(freq), ' [hz]']); % Update title
             end
-            updatePlot(obj, ax, index, plot_settings);
+            updatePlot(obj, index, plot_settings);
         end
 
 
         % Callback for first toggle button
-        function modifyLayout(obj, ax, lbl, labelTexts, sld, plot_settings)
+        function modifyLayout(obj, lbl, labelTexts, sld, plot_settings)
             global SPECTRUM;
 
             SPECTRUM = ~SPECTRUM;
             lbl.Text = labelTexts(SPECTRUM + 1);
-            cla(ax);
+            cla(obj.Axis);
 
             settings = plot_settings{SPECTRUM + 1};
             sld.Limits = settings{2};
             sld.Value = settings{6};
             sld.Step = settings{3};
 
-            xlim(ax, settings{1});
-            title(ax, settings{5}, ...
+            xlim(obj.Axis, settings{1});
+            title(obj.Axis, settings{5}, ...
                 'FontSize', 24, ...
                 'FontWeight','Bold', ...
                 'FontName', 'Times New Roman');
@@ -235,13 +234,13 @@ classdef DataApp < handle
             else
                 index = sld.Value;
             end
-            updatePlot(obj, ax, index, plot_settings);
+            updatePlot(obj, index, plot_settings);
 
         end
 
 
         % Callback for second toggle button
-        function modifyY(obj, ax, lbl1, labelTexts1, plot_settings, sld)
+        function modifyY(obj, lbl1, labelTexts1, plot_settings, sld)
             global SPECTRUM PHASE;
 
             PHASE = ~PHASE;
@@ -251,10 +250,10 @@ classdef DataApp < handle
             else
                 index = sld.Value; % Get current slider value\
             end
-            updatePlot(obj, ax, index, plot_settings);
+            updatePlot(obj, index, plot_settings);
         end
 
-        function updateScan(obj, src, ax, sld, plot_settings)
+        function updateScan(obj, src, sld, plot_settings)
             global DISPLAYED_SCANS SPECTRUM;
 
             if src.Value == 1
@@ -268,7 +267,7 @@ classdef DataApp < handle
             else
                 index = sld.Value; % Get current slider value\
             end
-            updatePlot(obj, ax, index, plot_settings);
+            updatePlot(obj, index, plot_settings);
         end
     end
 end
