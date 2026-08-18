6d556705f9eab301fca7bd8c059454a8af195e56
liador <liador@MacBook-Air-sl-Liad.local>
2026-08-18 21:15:47 +0200

    added settings object

---
+++ src/classes/Settings.m
@@ -0,0 +1,13 @@
+classdef Settings
+
+    properties (SetAccess = private)
+       XLim
+       SliderLim
+       SliderStep
+       SliderInit
+       PlotName
+       DataIndex
+       XData
+       YData
+    end
+end
